b7673e0e9e48d4142fba0c13dfbaf4445b659d09
yujilang <969489274@qq.com>
2017-10-16 19:54:01 +0800

    小改进 1，完善了静态文件的路径 2，完善了登录-注册页面之间的跳转

--- startproject/settings.py
+++ startproject/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #  Haystack 全文检索 配置
 HAYSTACK_CONNECTIONS = {
@@ -146,5 +149,5 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 #  通过 AUTH_USER_MODEL 指定自定义用户模型所在的位置
 #  AUTH_USER_MODEL = 'users.User'
 
-LOGOUT_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
