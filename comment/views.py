e6439da7529c8a45ce6e6548b2b7b878eb500ed8
yujilang <969489274@qq.com>
2017-10-19 19:32:02 +0800

    小修改 1，改错：在文章详情页的评论板块内缺少发表按钮 2，修改评论视图函数中作者信息的获取方法

--- comment/views.py
+++ comment/views.py
@@ -12,10 +12,8 @@ def post_comment(request, post_pk):
     #  这个函数的作用是当获取的文章（Post）存在时，则获取；否则返回 404 页面给用户。
     post = get_object_or_404(Post, pk=post_pk)
 
-    #  从 request 中获取 user 的 id
-    user_id = request.user.id
-    #  根据 user_id 从 User 模型中获取所有信息
-    user = get_object_or_404(User, pk=user_id)
+    #  从 request 中获取 user
+    user = request.user
 
     #  HTTP 请求有 get 和 post 两种，一般用户通过表单提交数据都是通过 post 请求，
     #  因此只有当用户的请求为 post 时才需要处理表单数据。
